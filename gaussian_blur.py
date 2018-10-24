48814822b5c5d0ddc6f84ee74835542b241f86f1
Gennady <dobrets@gmail.com>
2018-10-24 17:22:06 +0900

    gauss blur недокончено

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image
-from math import pi, log, exp
+from math import pi
 import numpy as np
 import sys
 
-def process(filename, r):
-    # должна обрабатывать файл filename гауссовым размытием в квадрате [-r, +r] x [-r, +r] 
+def gauss(filename, r):
+    # должна обрабатывать файл filename гауссовым размытием в квадрате [-r, +r] x [-r, +r]
     # и записывать результат в <filename>.gaussblurred.png
     img = Image.open(filename)
     img.load()
@@ -16,16 +16,28 @@ def process(filename, r):
     gauss_dist = np.exp( -(dx*dx+dy*dy)/(2*sigma**2) ) / (2*pi*sigma**2)
     coeff = gauss_dist / np.sum(gauss_dist)
 
+    for i in range(2*r+1):
+        for j in range(2*r+1):
+            print('{:.03f}'.format(gauss_dist[i,j]),end='')
+        print()
+
+    w, h = img.size
+    a = np.array(img.getdata(), dtype=np.uint8).reshape(h, w)
+    b = np.zeros((h, w), dtype=np.uint8)
+    for i in range(r+1,h-r-1-2700):
+        for j in range(r+1,w-r-1-2700):
+            for k in range(-r,r+1):
+                for m in range(-r,r+1):
+                    b[i,j] += coeff[k+r,m+r] * a[i+k,j+m]
     # код сюда ....
-    newimg = img
+    newimg = Image.fromarray(b)
     newimg.show()
     newimg.save(filename+'.gaussblurred.png')
-
 
 
 if __name__=='__main__':
     # Запускать с командной строки с аргументом <имя файла>, например: python gauss.py darwin.png
     if len(sys.argv) > 1:
-        main(sys.argv[1], 3)
+        gauss(sys.argv[1], 3)
     else:
         print("Must give filename.\n")

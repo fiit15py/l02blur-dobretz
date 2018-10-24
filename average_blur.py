c335190e63070a4c9964fb55de442e184766f2a2
Gennady <dobrets@gmail.com>
2018-10-24 15:52:26 +0900

    average blur

--- average_blur.py
+++ average_blur.py
@@ -20,12 +20,10 @@ def average_blur(img, r):
 img = Image.open('darwin.png')
 img.load()
 
-print("Image size is", img.size)
-print("Image mode is", img.mode)
+print("Image size:", img.size)
+print("Image mode:", img.mode)
 
-print("getdata[1]=", img.getdata()[1])
 a = np.array(img, dtype=np.uint8).reshape(img.size[::-1])
-print("a[0,1]=", a[0,1])
 
 # b = a[1000:2000, 1000:2000]
 pic = Image.fromarray(a)
